6a149c934f357fcdbd8cf4ee0f5be992f6cbbb4f
christopher <christopher.cortes15@inacapmail.cl>
2026-06-24 17:16:49 -0400

    componente recetacard con props y validacion

--- recetapp-react/src/components/ListaRecetas.jsx
+++ recetapp-react/src/components/ListaRecetas.jsx
@@ -10,7 +10,15 @@ export const ListaRecetas = ({ recetas }) => {
       ) : (
         <div className="recetas-grid">
           {recetas.map((receta) => (
-            <RecetaCard key={receta.id} receta={receta} />
+            <RecetaCard
+              key={receta.id}
+              nombre={receta.nombre}
+              origen={receta.origen}
+              porciones={receta.porciones}
+              categoria={receta.categoria}
+              descripcion={receta.descripcion}
+              ingredientes={receta.ingredientes}
+            />
           ))}
         </div>
       )}
